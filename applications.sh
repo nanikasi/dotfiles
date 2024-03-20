892e6fb738a8c4c11430261cdc2bc25e199de560
Naoto Nakanishi <nanikasi1024@gmail.com>
2024-03-20 15:08:04 +0900

    change applications

--- applications.sh
+++ applications.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -eu
 
 # BROWSER="google-chrome"
-# PROG="appcleaner notion orbstack raycast rectangle karabiner-elements discord slack the-unarchiver visual-studio-code wezterm"
-
 BROWSER=""
-PROG="rectangle"
-brew install -f $BROWSER $PROG
+PROG="appcleaner cheatsheet notion orbstack raycast discord slack visual-studio-code wezterm alt-tab figma kindle zoom"
+
+brew install $BROWSER $PROG
