1e0ff1edfa605094e6c08ec40f3ca3a68489260b
Naoto Nakanishi <nanikasi1024@gmail.com>
2024-03-20 15:08:29 +0900

    add wezterm

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 ln -snfv ${PWD}/.zprofile ~/.zprofile
 ln -snfv ${PWD}/.zshrc ~/.zshrc
+ln -snfv ${PWD}/.gitconfig_shared ~/.gitconfig_shared
+ln -snfv ${PWD}/.gitignore_global ~/.gitignore_global
+ln -snfv ${PWD}/.wezterm.lua ~/.wezterm.lua
 
 git config --global include.path "~/.gitconfig_shared"
-git config --global include.path "~/.gitconfig_shared"
+git config --global core.excludesfile "~/.gitignore_global"
 
 command echo "Setup completed"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
