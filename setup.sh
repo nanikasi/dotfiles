c12e5244f2e6f42cfe27d8cb5b6f20c8bafb72c7
Naoto Nakanishi <nanikasi1024@gmail.com>
2024-03-23 14:52:18 +0900

    mv brew install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -eu
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ln -snfv ${PWD}/.zprofile ~/.zprofile
 ln -snfv ${PWD}/.zshrc ~/.zshrc
 ln -snfv ${PWD}/.gitconfig_shared ~/.gitconfig_shared
@@ -10,6 +13,3 @@ git config --global include.path "~/.gitconfig_shared"
 git config --global core.excludesfile "~/.gitignore_global"
 
 command echo "Setup completed"
-
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
