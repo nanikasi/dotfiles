770e54d0e9235a77f95bf32c09983c60a4526bb0
Naoto Nakanishi <nanikasi1024@gmail.com>
2024-03-23 13:46:37 +0900

    add mise and edit applications.sh

--- applications.sh
+++ applications.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
 
 BROWSER="google-chrome"
-PROG="appcleaner cheatsheet notion orbstack raycast discord slack visual-studio-code wezterm alt-tab fig figma zoom asdf"
-CMD="eza git-delta"
+PROG="appcleaner cheatsheet notion orbstack raycast discord slack visual-studio-code wezterm alt-tab figma zoom"
+CMD="eza git-delta mise"
 
 brew install $BROWSER $PROG $CMD
